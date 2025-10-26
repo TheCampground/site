b4bbee94f845f451c9f365493905523fe414502a
Brad <44570552+bizojs@users.noreply.github.com>
2025-10-26 14:49:04 +0000

    refactor: favicon and assets

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,13 +1,12 @@
 <script lang="ts">
     import Button from "@core/components/Button.svelte"
-    import { Tipi } from "@steeze-ui/phosphor-icons"
-    import { Icon } from "@steeze-ui/svelte-icon"
+    import Campground from "@core/assets/favicon.png"
 </script>
 
 <header class="sticky top-4 border border-on-surface/10 card flex items-center justify-between gap-2 h-12 pl-4 pr-2.5 flex-self-stretch rounded-xl bg-container-low/70 backdrop-blur-xs text-on-surface flex-grow w-full shadow-lg z-[999]">
     <div class="flex items-center gap-6">
         <Button href="/" element="#root" variant="link">
-            <Icon src={Tipi} theme="fill" class="size-6" />
+            <img src={Campground} alt="Campground" class="h-5 w-auto select-none pointer-events-none" draggable="false" />
         </Button>
         <Button href="/" element="#projects" variant="link">
             Projects
