2208a25004a52622da2e49c3b4ed98d063ce323d
Brad <44570552+bizojs@users.noreply.github.com>
2025-10-26 14:48:13 +0000

    refactor: projects component

--- src/lib/components/Project.svelte
+++ src/lib/components/Project.svelte
@@ -1,13 +1,12 @@
 <script lang="ts">
     import { Icon, type IconSource } from "@steeze-ui/svelte-icon"
+    import { GithubLogo, Tipi } from "@steeze-ui/phosphor-icons"
     import Campground from "@core/assets/campground.png"
+    import Button from "@core/components/Button.svelte"
     import type { ProjectCardProps } from "@core/types"
-    import { Tipi } from "@steeze-ui/phosphor-icons"
     import { langColors } from "@core/utils"
 
-    const { name, description, image, lang, extra = [] }: ProjectCardProps = $props()
-
-    const color = langColors[lang as string] ?? "var(--primary)"
+    const { name, description, image, lang, langs, extra = [], link }: ProjectCardProps = $props()
 </script>
 
 <div class="card flex flex-col gap-3 p-4 flex-self-stretch rounded-xl bg-container-low text-on-surface flex-grow w-full shadow-lg">
@@ -20,15 +19,27 @@
     </div>
     <div class="flex gap-1 items-center flex-wrap flex-1">
         {#if lang}
+            {@const color = langColors[lang as string] ?? "var(--primary)"}
             {@render tag(color, lang)}
+        {/if}
+        {#if langs}
+            {#each langs as lang}
+                {@const color = langColors[lang as string] ?? "var(--primary)"}
+                {#if lang}
+                    {@render tag(color, lang)}
+                {/if}
+            {/each}
         {/if}
         {#each extra as item}
             {@render tag(item.icon, item.value)}
         {/each}
     </div>
-    <div class="flex items-center gap-2 text-on-surface-alt select-none">
-        <Icon src={Tipi} theme="fill" class="size-4" />
-        <p class="text-xs">Built on the Campground</p>
+    <div class="flex items-center w-full justify-between">
+        <div class="flex items-center gap-2 text-on-surface-alt select-none">
+            <Icon src={Tipi} theme="fill" class="size-4" />
+            <p class="text-xs">Built on the Campground</p>
+        </div>
+        {@render projectLink()}
     </div>
 </div>
 
@@ -41,4 +52,21 @@
         {/if}
         <span class="text-[0.7rem] font-medium">{value}</span>
     </div>
+{/snippet}
+
+{#snippet projectLink()}
+    {#if link}
+        {@const url = link.type === "github" ? `https://github.com/TheCampground/${link.repo}` : link.url}
+        <Button href={url} variant="default" class="flex items-center gap-1">
+            {#if link.type === "github"}
+                <Icon src={GithubLogo} theme="bold" class="size-4" />
+                GitHub
+            {:else}
+                {#if link.icon}
+                    <Icon src={link.icon} theme="bold" class="size-4" />
+                {/if}
+                {link.text}
+            {/if}
+        </Button>
+    {/if}
 {/snippet}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,13 @@
 <script lang="ts">
-    import { Star, Users, UsersThree } from "@steeze-ui/phosphor-icons"
+    import { UsersThree, GithubLogo, HandWaving } from "@steeze-ui/phosphor-icons"
     import Marshmallow from "@core/assets/Marshmallow.png"
     import TheCampsite from "@core/assets/TheCampsite.png"
     import Project from "@core/components/Project.svelte"
+    import Button from "@core/components/Button.svelte"
+    import Campground from "@core/assets/favicon.png"
     import Card from "@core/components/Card.svelte"
-    import Chip from "@core/components/Chip.svelte"
+    import Bonfire from "@core/assets/bonfire.png"
+    import { Icon } from "@steeze-ui/svelte-icon"
 </script>
 
 <main id="home" class="flex content-center w-full">
@@ -13,9 +16,10 @@
         header="Hey! We are Campground."
         description="Campground is an independent collective where creativity meets code and design. We're a small group passionate about developing open-source tools, experimenting with new tech, and growing projects under one shared identity. Whether it's a new app, community, or a weekend experiment, every project developed at Campground is fueled by curiosity, collaboration, and s'mores."
     >
-        <Chip icon={Users}>
-            2
-        </Chip>
+        <Button href="https://github.com/TheCampground" variant="default" class="flex items-center gap-1 p-2">
+            <Icon src={GithubLogo} theme="bold" class="size-4" />
+            GitHub
+        </Button>
     </Card>
 </main>
 
@@ -28,20 +32,28 @@
             name="The Campsite"
             description="An ever-growing community on Discord that aims to provide a warm welcome to all new members joining us!"
             image={TheCampsite}
-            extra={[{ icon: UsersThree, value: 300 }]}
+            extra={[{ icon: UsersThree, value: 23 }]}
+            link={{ type: "url", text: "Join Us!", icon: HandWaving, url: "/join" }}
         />
-        <Project
+        <!-- If / when Shuttle is recreated to match the theme -->
+        <!-- <Project
             name="Bonfire"
             description="A cute, fun and easy to use ShareS replacement."
-            lang="Kotlin"
-            extra={[{ icon: Star, value: 14 }]}
-        />
+            image={Bonfire}
+            langs={["Kotlin", "Svelte", "TypeScript"]}
+        /> -->
         <Project
             name="Marshmallow"
-            description="An open-source Discord bot written in TypeScript for The Campsite community"
+            description="The helper bot for The Campsite Discord community"
             image={Marshmallow}
             lang="TypeScript"
-            extra={[{ icon: Star, value: 14 }]}
+        />
+        <Project
+            name="Campground Theme"
+            description="A custom Discord / Vencord theme to match the aesthetic of Campground (WIP)"
+            image={Campground}
+            lang="CSS"
+            link={{ type: "github", repo: "campground-theme" }}
         />
     </div>
 </div>
